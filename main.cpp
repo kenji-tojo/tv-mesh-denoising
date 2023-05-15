6393d7555862520d84441ce74280ec6505111501
Kenji Tojo <kenjitojo@icloud.com>
2023-05-15 14:55:35 +0900

    add denoiser

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <filesystem>
 
 #include <Eigen/Dense>
 #include "denoiser/denoiser.hpp"
 #include "cxxopts/cxxopts.hpp"
 #include "igl/readOBJ.h"
+#include "igl/writeOBJ.h"
+#include "igl/per_face_normals.h"
 
 
 int main(int argc, char *argv[]) {
@@ -14,10 +17,19 @@ int main(int argc, char *argv[]) {
     options.parse_positional({"obj"});
     auto args = options.parse(argc, argv);
 
-    tvd::hello(args["obj"].as<std::string>());
-
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
     igl::readOBJ(args["obj"].as<std::string>(), V, F);
-    std::cout << V.rows() << " vertices and " << F.rows() << " triangles" << std::endl;
+    std::cout << V.rows() << " vertices and " << F.rows() << " triangles." << std::endl;
+
+
+    Eigen::MatrixXd iniN, N;
+    igl::per_face_normals(V, F, iniN);
+    auto denoiser = tvd::Denoiser<double>(V, F);
+    denoiser.alpha = 100.0;
+    denoiser.denoise(iniN, N, /*n_iter=*/70);
+    tvd::Denoiser<double>::recover_positions(F, N, V);
+
+    std::filesystem::create_directory("./result");
+    igl::writeOBJ("./result/denoised.obj", V, F);
 }
